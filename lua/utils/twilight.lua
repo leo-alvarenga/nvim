e0e4a4b307e4602cc59eb1b33bfbb0511be4aa00
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2025-12-10 11:25:56 -0300

    feat: add twilight status notification

--- lua/utils/twilight.lua
+++ lua/utils/twilight.lua
@@ -7,7 +7,14 @@ M.enabled = false
 function M.toggle_twilight()
 	M.enabled = not M.enabled
 
+	local prefix = "[Twilight] "
+	local status = prefix .. "Code blocks highlighting enabled"
+	if not M.enabled then
+		status = prefix .. "Code block highlighting disabled"
+	end
+
 	vim.cmd(_shared.twilight.cmd)
+	vim.notify(status, vim.log.levels.INFO)
 end
 
 return M
