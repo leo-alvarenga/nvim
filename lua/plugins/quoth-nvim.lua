f8f4291621187b5524f6ca20bae8de4f7b074133
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2025-12-15 07:45:23 -0300

    fix: limit quote length to minimize issues on WSL

--- lua/plugins/quoth-nvim.lua
+++ lua/plugins/quoth-nvim.lua
@@ -1,6 +1,12 @@
 return {
-	-- "leo-alvarenga/quoth.nvim",
-	dir = "~/dev/quoth.nvim",
-	opts = {},
+	"leo-alvarenga/quoth.nvim",
+	-- dir = "~/dev/quoth.nvim",
+	opts = {
+		filter = {
+			length_constraints = {
+				max = 80,
+			},
+		},
+	},
 	branch = "nightly",
 }
