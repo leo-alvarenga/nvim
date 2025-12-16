626f7b76ef72917b340b8b466c5cfd8af303c849
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2025-12-16 13:35:19 -0300

    feat: undo quoth.nvim config

--- lua/plugins/quoth-nvim.lua
+++ lua/plugins/quoth-nvim.lua
@@ -1,12 +1,11 @@
 return {
-	-- "leo-alvarenga/quoth.nvim",
-	dir = "~/dev/quoth.nvim",
+	"leo-alvarenga/quoth.nvim",
+	-- dir = "~/dev/quoth.nvim",
 	opts = {
 		filter = {
-			authors = { "Seneca", "albert" },
-			tags = { "abc", "value" },
-			tag_mode = "and",
-			relax_author_search = true,
+			length_constraints = {
+				max = 80,
+			},
 		},
 	},
 	branch = "nightly",
