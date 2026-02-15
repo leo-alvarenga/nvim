d70e9c863f1dfa9c2c6c0c302fdab200b3128936
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2026-02-15 07:33:14 -0300

    feat: add context and ufo

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -5,6 +5,7 @@ return {
 	require("plugins.lspconfig.mason-tool-installer"),
 	require("plugins.lspconfig.mason"),
 	require("plugins.lspconfig.none-ls"),
+	require("plugins.lspconfig.nvim-treesitter-context"),
 	require("plugins.lspconfig.treesitter"),
 	require("plugins.lspconfig.typescript-tools"),
 }
