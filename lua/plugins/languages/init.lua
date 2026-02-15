40c7ed0a871d60814ad65292abf855dee5363027
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2026-02-15 02:09:17 -0300

    feat: move all plugins to new dir

--- lua/plugins/languages/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-	require("plugins.languages.blink"),
-	require("plugins.languages.conform"),
-	require("plugins.languages.none-ls"),
-	require("plugins.languages.treesitter"),
-	require("plugins.languages.tiny-inline-diagnostic"),
-	require("plugins.languages.trouble"),
-}
