712166449d2087064c1d2c70af96b8df859742e3
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2026-02-19 17:37:54 -0300

    feat: move twilight and conform functions to the plugins files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,7 +1,57 @@
-local static_web_fmt_combo = { "prettier", "ast-grep", stop_after_first = true }
-local web_fmt_combo = { "prettier", stop_after_first = true }
+local M = {}
 
-local opts = {
+M.format_on_save = require("values.workflow").format_on_save
+
+--- Formats the current buffer using Conform
+--- @param args table: Arguments to pass to Conform's format function
+--- @param silent boolean: Whether to suppress notifications (default: false)
+function M.format_current(args, silent)
+	if not silent then
+		vim.notify(require("values.constants.shared").format.notifications.manual, vim.log.levels.INFO)
+	end
+
+	require("conform").format(args)
+end
+
+--- Toggles the format on save feature and notifies the user of the new status
+function M.toggle_format_on_save()
+	M.format_on_save = not M.format_on_save
+
+	local _shared = require("values.constants.shared")
+
+	local status = "enabled"
+
+	if not M.format_on_save then
+		status = "disabled"
+	end
+
+	vim.notify(_shared.format.notifications[status], vim.log.levels.INFO)
+end
+
+--- Checks if format on save is enabled and formats the current buffer if it is
+--- @param args table: Arguments to pass to Conform's format function
+function M.format_if_enabled(args)
+	if not M.format_on_save then
+		return
+	end
+
+	M.format_current(args, true)
+end
+
+--- Sets up an autocmd to format the buffer before saving if format on save is enabled
+function M.setup_autocmd()
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*",
+		callback = function(args)
+			M.format_if_enabled({ bufnr = args.buf })
+		end,
+	})
+end
+
+M.static_web_fmt_combo = { "prettier", "ast-grep", stop_after_first = true }
+M.web_fmt_combo = { "prettier", stop_after_first = true }
+
+M.opts = {
 	formatters_by_ft = {
 		-- Basics
 		bash = { "shellharden", "shfmt", stop_after_first = true },
@@ -14,16 +64,16 @@ local opts = {
 		yaml = { "yamlfmt", "prettier", "helm_ls" },
 
 		-- Web dev
-		css = static_web_fmt_combo,
-		scss = static_web_fmt_combo,
-		html = static_web_fmt_combo,
-		htmx = static_web_fmt_combo,
-		jsx = web_fmt_combo,
-		tsx = web_fmt_combo,
-		javascriptreact = web_fmt_combo,
-		typescriptreact = web_fmt_combo,
-		javascript = web_fmt_combo,
-		typescript = web_fmt_combo,
+		css = M.static_web_fmt_combo,
+		scss = M.static_web_fmt_combo,
+		html = M.static_web_fmt_combo,
+		htmx = M.static_web_fmt_combo,
+		jsx = M.web_fmt_combo,
+		tsx = M.web_fmt_combo,
+		javascriptreact = M.web_fmt_combo,
+		typescriptreact = M.web_fmt_combo,
+		javascript = M.web_fmt_combo,
+		typescript = M.web_fmt_combo,
 
 		-- Misc
 		rust = { "ast-grep" },
@@ -34,18 +84,17 @@ return {
 	-- Apply formatters
 	"stevearc/conform.nvim",
 	config = function()
-		require("conform").setup(opts)
+		require("conform").setup(M.opts)
 
 		local _keymap = require("config.utils.keymap")
 
 		local map = _keymap.map
 		local with_prefix = _keymap.with_prefix
 
-		local _format = require("config.utils.format")
-		_format.setup_autocmd()
+		M.setup_autocmd()
 
 		map({ "", "i" }, with_prefix("", "formatters"), "", "   Formatter actions")
-		map({ "", "i" }, with_prefix("s", "formatters"), _format.format_current, "Format file (if possible)")
-		map({ "", "i" }, with_prefix("S", "formatters"), _format.toggle_format_on_save, "Toggle format on save")
+		map({ "", "i" }, with_prefix("s", "formatters"), M.format_current, "Format file (if possible)")
+		map({ "", "i" }, with_prefix("S", "formatters"), M.toggle_format_on_save, "Toggle format on save")
 	end,
 }
