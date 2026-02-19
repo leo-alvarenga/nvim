205703595b807a338d94fe7eb9f58b29faef0d4c
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2026-02-19 17:29:23 -0300

    feat: move all keymaps to plugin files for better code splitting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,35 +1,51 @@
 local static_web_fmt_combo = { "prettier", "ast-grep", stop_after_first = true }
 local web_fmt_combo = { "prettier", stop_after_first = true }
 
+local opts = {
+	formatters_by_ft = {
+		-- Basics
+		bash = { "shellharden", "shfmt", stop_after_first = true },
+		json = { "prettier", "jq", stop_after_first = true },
+		lua = { "stylua" },
+		markdown = { "prettier" },
+
+		-- DevOps and Infra
+		terraform = { "terraform" },
+		yaml = { "yamlfmt", "prettier", "helm_ls" },
+
+		-- Web dev
+		css = static_web_fmt_combo,
+		scss = static_web_fmt_combo,
+		html = static_web_fmt_combo,
+		htmx = static_web_fmt_combo,
+		jsx = web_fmt_combo,
+		tsx = web_fmt_combo,
+		javascriptreact = web_fmt_combo,
+		typescriptreact = web_fmt_combo,
+		javascript = web_fmt_combo,
+		typescript = web_fmt_combo,
+
+		-- Misc
+		rust = { "ast-grep" },
+	},
+}
+
 return {
 	-- Apply formatters
 	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			-- Basics
-			bash = { "shellharden", "shfmt", stop_after_first = true },
-			json = { "prettier", "jq", stop_after_first = true },
-			lua = { "stylua" },
-			markdown = { "prettier" },
+	config = function()
+		require("conform").setup(opts)
 
-			-- DevOps and Infra
-			terraform = { "terraform" },
-			yaml = { "yamlfmt", "prettier", "helm_ls" },
+		local _keymap = require("config.utils.keymap")
 
-			-- Web dev
-			css = static_web_fmt_combo,
-			scss = static_web_fmt_combo,
-			html = static_web_fmt_combo,
-			htmx = static_web_fmt_combo,
-			jsx = web_fmt_combo,
-			tsx = web_fmt_combo,
-			javascriptreact = web_fmt_combo,
-			typescriptreact = web_fmt_combo,
-			javascript = web_fmt_combo,
-			typescript = web_fmt_combo,
+		local map = _keymap.map
+		local with_prefix = _keymap.with_prefix
 
-			-- Misc
-			rust = { "ast-grep" },
-		},
-	},
+		local _format = require("config.utils.format")
+		_format.setup_autocmd()
+
+		map({ "", "i" }, with_prefix("", "formatters"), "", "   Formatter actions")
+		map({ "", "i" }, with_prefix("s", "formatters"), _format.format_current, "Format file (if possible)")
+		map({ "", "i" }, with_prefix("S", "formatters"), _format.toggle_format_on_save, "Toggle format on save")
+	end,
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,23 @@
 return {
 	"stevearc/oil.nvim",
+
+	config = function()
+		require("oil").setup({})
+
+		local _keymap = require("config.utils.keymap")
+		local _shared = require("values.constants.shared")
+
+		local map = _keymap.map
+		local with_prefix = _keymap.with_prefix
+
+		map("", with_prefix("e", "pickers"), _keymap.to_cmd(_shared.oil.cmd), "Manage the current directory using Oil")
+	end,
+
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
 
 	lazy = false,
-	opts = {},
 
 	version = false,
 }
