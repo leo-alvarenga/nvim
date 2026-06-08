ee17dcd5e4c0d014f1c73ea092aafcd83a18d7da
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2026-06-08 09:38:01 -0300

    fix: refresh linter on buffer update

--- lua/plugins/code-completions/nvim-lint.lua
+++ lua/plugins/code-completions/nvim-lint.lua
@@ -42,6 +42,12 @@ return {
 					rust = { "ast-grep" },
 				},
 			})
+
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
 		end,
 	},
 }
